548ad9ce01f24b06a13ad81e616a25ec7a86c509
juan cesar andrianto <juancesarandrianto@gmail.com>
2020-04-30 16:49:50 +0700

    remove auth in lelang

--- prelovedapi/src/services/lelang/lelang.hooks.js
+++ prelovedapi/src/services/lelang/lelang.hooks.js
@@ -1,8 +1,8 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
-
+//authenticate('jwt')
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
+    all: [  ],
     find: [],
     get: [],
     create: [],
